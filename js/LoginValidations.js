6bdc397047f30add0d2591ed62db817995568fcb
statavarthy <statavarthy@luc.edu>
2015-10-08 15:55:07 -0500

    Included validations for sign up Page

--- js/LoginValidations.js
+++ js/LoginValidations.js
@@ -1,29 +1,45 @@
-/**
- * 
- */
+//Problem: Hints are shown even when form is valid
+//Solution: Hide and show them at appropriate times
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
 
-function resetData(input) {
-	document.getElementById(input).value="";
+//Hide hints
+$("form span").hide();
+
+function passwordEvent(){
 	
+    //Find out if password is valid  
+    if($password.val().length > 5) {
+      //Hide hint if valid
+      $password.next().hide();
+    } else {
+      //else show hint
+      $password.next().show();
+    }
 }
 
-function createNewAccount() 
-{
-	
-	if((document.getElementById("userName").value.indexOf('@')==-1) || (document.getElementById("userName").value.indexOf('.')==-1) || (document.getElementById("userName").value==""))
-	{
-		alert("Please enter a valid Email ID");
-	}
-	
-	else if(document.getElementById("pswrd").value != document.getElementById("pswrdVer2").value)
-	{
-		alert("Passwords do not match!!");
-	}
-	
-	else
-	{
-		alert("Account Created successfully!!");
-	}
-	
-	
+function confirmPasswordEvent() {
+  //Find out if password and confirmation match
+  if($password.val() === $confirmPassword.val()) {
+    //Hide hint if match
+    $confirmPassword.next().hide();
+  } else {
+    //else show hint 
+    $confirmPassword.next().show();
+  }
 }
+
+
+//When event happens on password input
+$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+//When event happens on confirmation input
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+
+
+
+
+
+
+
+
+
