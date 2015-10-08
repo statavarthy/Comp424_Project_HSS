68023cb7a175a4bbc546e8ca68470980077a0167
henry2992 <henry2992@hotmail.com>
2015-10-08 18:34:45 -0500

    git finish navbar mobile

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//      $("#submenu").hide();
+//   });
+//
+//   $("#mainmenu").click(function() {
+//       if($("#submenu").is(":hidden")){
+//           $("#submenu").show("slow");
+//           $(".Menu").text("Close Menu");
+//           $(".Menu").removeClass( "Menu" ).addClass( "CloseMenu" );
+//       }
+//       else{
+//           $("#submenu").hide();
+//           $(".CloseMenu").text("Menu");
+//           $(".CloseMenu").removeClass( "CloseMenu" ).addClass( "Menu" );
+//       }
+//   });
