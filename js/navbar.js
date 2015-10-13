db2c8c2424f5e63f5ae08cd01cbb0914af0b094d
statavarthy <statavarthy@luc.edu>
2015-10-13 10:59:20 -0500

    Separated the code in js file

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,18 @@
-// $(document).ready(function() {
-//      $("#submenu").hide();
-//   });
-//
-//   $("#mainmenu").click(function() {
-//       if($("#submenu").is(":hidden")){
-//           $("#submenu").show("slow");
-//           $(".Menu").text("Close Menu");
-//           $(".Menu").removeClass( "Menu" ).addClass( "CloseMenu" );
-//       }
-//       else{
-//           $("#submenu").hide();
-//           $(".CloseMenu").text("Menu");
-//           $(".CloseMenu").removeClass( "CloseMenu" ).addClass( "Menu" );
-//       }
-//   });
+	$(document).ready(function() {		
+	     $("#submenu").hide();
+	  });
+
+	  $("#mainmenu").click(function() {
+	      if($("#submenu").is(":hidden")){
+	          $("#submenu").show("slow");
+	          // $(".Menu").text("Close Menu");
+	          // $(".Menu").removeClass( "Menu" ).addClass( "CloseMenu" );
+	      }
+	      else{
+	          $("#submenu").hide();
+	          // $(".CloseMenu").text("Menu");
+	          // $(".CloseMenu").removeClass( "CloseMenu" ).addClass( "Menu" );
+	      }
+	  });
+
+
